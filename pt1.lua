78dfa5277ca31ecab75e14a33591a075bccfb471
LangitDeveloper <alfahriridhoramdhani1@gmail.com>
2026-01-21 21:17:25 +0700

    Add files via upload

--- pt1.lua
+++ pt1.lua
@@ -1147,7 +1147,7 @@ FishV2Tab:AddSection({Name = "Fishing Features"})
 local function Fastest()
     task.spawn(function()
         pcall(function()
-            Network.Functions.CancelFishing:InvokeServer()
+            Network.Functions.CancelFish:InvokeServer()
         end)
         
         pcall(function()
